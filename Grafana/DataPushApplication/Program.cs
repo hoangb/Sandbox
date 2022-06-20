cc1e94c3d7d03517394bac0e96f1b75fcb0b31d4
hoang.bao <hoangbao91770@gmail.com>
2022-06-20 04:23:08 -0700

    Add test code for a new metrics gauge

--- Grafana/DataPushApplication/Program.cs
+++ Grafana/DataPushApplication/Program.cs
@@ -1,4 +1,10 @@
+using App.Metrics;
+using App.Metrics.Formatters.Prometheus;
+using App.Metrics.Gauge;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+IMetricsRoot metrics;
 
 // Add services to the container.
 
@@ -7,7 +13,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMetrics(metrics = AppMetrics.CreateDefaultBuilder().Build());
+
 var app = builder.Build();
+
+
+
+app.UseMetricsAllMiddleware();
+app.UseMetricsEndpoint(new MetricsPrometheusTextOutputFormatter());
+
+var gauge = new GaugeOptions
+{
+    Name = "My gauge",
+    MeasurementUnit = Unit.Calls
+};
+
+metrics.Measure.Gauge.SetValue(gauge, 1.1);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
